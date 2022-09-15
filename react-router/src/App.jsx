523355ebff33f9eeb48c81f95591e68a66958643
Camperoh7 <gibran_h7@hotmail.com>
2022-09-14 20:48:18 -0500

    navbar!

--- react-router/src/App.jsx
+++ react-router/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import RoutesIndex from './routes'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
     <div className='App'> 
+      <Navbar />
       <RoutesIndex />
     </div>
   )
