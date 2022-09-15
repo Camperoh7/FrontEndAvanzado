a700605e533053ab42ec50ab757aa117c03754a5
Camperoh7 <gibran_h7@hotmail.com>
2022-09-14 20:40:27 -0500

    rutas básicas con react router dom

---
+++ react-router/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import logo from '../assets/react.svg';
+
+const Home = () => {
+    return (
+        <>
+        <img src={logo} classname='App-logo' alt='Logo' />
+        <h1>Home</h1>
+        </>
+    )
+}
+
+export default Home
