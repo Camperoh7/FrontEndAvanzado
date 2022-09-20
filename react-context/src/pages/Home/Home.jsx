c1a1c4ab04190f595a475b28ee73794405bab510
Camperoh7 <gibran_h7@hotmail.com>
2022-09-19 20:52:49 -0500

    Context Básico

---
+++ react-context/src/pages/Home/Home.jsx
@@ -0,0 +1,20 @@
+import { SongList } from '@/components/SongList'
+import { SongDetail } from '@/components/SongDetail'
+import './home.css'
+
+const Home = () => {
+    return (
+        <div className='home-container'>
+            <div className='izquierdo'>
+                <h2>Lado Izquierdo</h2>
+                <SongList />
+            </div>
+            <div className='derecho'>
+                <h2>Lado Derecho</h2>
+                <SongDetail />
+            </div>
+        </div>
+    )
+}
+
+export default Home
